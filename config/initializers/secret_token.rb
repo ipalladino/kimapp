522f70d193c898ad1339c6b089a7906a22d99cb5
Ignacio Palladino <ignacio@flingo.tv>
2013-11-04 22:00:34 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KimApp::Application.config.secret_key_base = '37c393db3fe5865ba788c70616b0cb6b9587932e70e531ba703b38c86a53394d79661eaa0bb38e22698f7edc8eacb73d59cc14376e77ab6b0a0886303971b169'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KimApp::Application.config.session_store :cookie_store, key: '_kim_app_session'
